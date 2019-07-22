153d0fd064c9f07f50d773c4704b6cd9db4b7e36
ilyes12zouaoui <ilyes.zouaoui@esprit.tn>
2019-07-22 19:29:37 +0200

    Updates

---
+++ precache-manifest.1aeb0ccee413ccf2a9d8b8c27384ebaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ac0f0bbdbb27486dec08c8bf8050527",
+    "url": "/ilyes-zouaoui-redux-CRUD/index.html"
+  },
+  {
+    "revision": "a4d75c6819d113219270",
+    "url": "/ilyes-zouaoui-redux-CRUD/static/css/main.9187091b.chunk.css"
+  },
+  {
+    "revision": "d3166d654e8be71d8a94",
+    "url": "/ilyes-zouaoui-redux-CRUD/static/js/2.7f2297d1.chunk.js"
+  },
+  {
+    "revision": "a4d75c6819d113219270",
+    "url": "/ilyes-zouaoui-redux-CRUD/static/js/main.dd7df1cc.chunk.js"
+  },
+  {
+    "revision": "b4f4b4732a14e8b3a52a",
+    "url": "/ilyes-zouaoui-redux-CRUD/static/js/runtime~main.84836a3c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ilyes-zouaoui-redux-CRUD/static/media/logo.5d5d9eef.svg"
+  }
+]);
